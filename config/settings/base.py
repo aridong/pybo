37300481b4e91e390c1a54909a11d314b6f7e27e
이상민 <aridong75@gmail.com>
2023-07-24 11:10:38 +0900

    log 생성

--- config/settings/base.py
+++ config/settings/base.py
@@ -154,9 +154,6 @@ LOGGING = {
             'style': '{',
         },
     },
-    'standard': {
-        'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-    },
     'handlers': {
         'console': {
             'level': 'INFO',
@@ -173,19 +170,10 @@ LOGGING = {
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         },
-        'file': {
-            'level': 'INFO',
-            'filters': ['require_debug_false'],
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': BASE_DIR / 'logs/mysite.log',
-            'maxBytes': 1024 * 1024 * 5,  # 5 MB
-            'backupCount': 5,
-            'formatter': 'standard',
-        },
     },
     'loggers': {
         'django': {
-            'handlers': ['console', 'mail_admins', 'file'],
+            'handlers': ['console', 'mail_admins'],
             'level': 'INFO',
         },
         'django.server': {
